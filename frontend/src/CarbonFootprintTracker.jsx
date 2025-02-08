4319f059c9038bdb595ff45dd6cf5f16ddf31806
Faizal Rayyan (2023) ZMAC412 <rayyan.faizal.2023@live.rhul.ac.uk>
2025-02-08 21:21:55 +0000

    #17 fixed alignment

--- frontend/src/CarbonFootprintTracker.jsx
+++ frontend/src/CarbonFootprintTracker.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css'
+import './CarbonFootprintTracker.css'
 
 const CarbonFootprintTracker = () => {
   const [answers, setAnswers] = useState({
